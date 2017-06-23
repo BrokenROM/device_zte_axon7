a1ba75532bc5b306a2fb2381bb749aa13cdf1617
dianlujitao <dianlujitao@lineageos.org>
2017-06-23 09:35:20 +0800

    axon7: Sync bluetooth configuration with CAF
    Change-Id: I00e83f5cecf01171cf2f309f02c487e608a4d8be
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,6 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "Axon 7"
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
